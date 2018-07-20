fbdda7d41a47bb580dec5ee8d40be002074dffec
mariliamonteiro <31514228+mariliamonteiro@users.noreply.github.com>
2018-07-20 15:57:06 -0300

    Formulario Completo! Funcionando e salvando o arquivo na pasta "temp_files".

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+import os
 from flask import Flask, render_template, url_for, redirect, request
+from flask_uploads import UploadSet, configure_uploads, DATA, patch_request_class
 from forms import FormARIMA
 from variables import *
+from werkzeug.utils import secure_filename
 
 # VARIAVEIS DE INICIALIZACAO ===================================================
 app = Flask(__name__)
 desc_list = longDesc()
 
 app.config['SECRET_KEY'] = 'd5fda74dcf2c2fdbfca06a4ebfc65b86a9e0da08d0115dce61f522f183b156d8'
+app.config['UPLOADED_FILES_DEST'] = 'temp_files'
+
+files = UploadSet('files', DATA)
+configure_uploads(app, files)
+patch_request_class(app)
+
+UPLOAD_FOLDER = 'temp_files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # PAGINAS PRINCIPAIS ===========================================================
 @app.route('/')
@@ -26,14 +37,36 @@ def algorithms():
 @app.route('/algorithms/arima', methods= ['GET', 'POST'])
 def algorithms_arima():
     text = desc_list['arima']
-    form = FormARIMA(request.form)
 
-    if (request.method == 'POST') and form.validate_on_submit():
-        if form.data.data:
-            result = form.data.filename
-            return render_template('algorithms_arima_output.html', title='ARIMA', text=text, form= form, result= result)
+    #form = FormARIMA(request.form)
+    form = FormARIMA()
+
+    if form.validate_on_submit():
+        #file = request.files['dados']
+
+        filename = secure_filename(form.dados.data.filename)
+        form.dados.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+        #filename = form.dados.data.filename
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #form.dados.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #filename = files.save(form.dados.data)
+        file_url = files.url(form.dados.data.filename)
+        return render_template('algorithms_arima_output.html', title='ARIMA', text=text, form= form, file_url=file_url)
     else:
-        return render_template('algorithms_arima.html', title='ARIMA', text=text, form= form)
+        file_url = None
+
+    return render_template('algorithms_arima.html', title='ARIMA', text=text, form= form, file_url=file_url)
+
+
+    #
+    # if (request.method == 'POST') and form.validate_on_submit():
+    #     if form.data.data:
+    #         result = form.data.filename
+    #         return render_template('algorithms_arima_output.html', title='ARIMA', text=text, form= form, result= result)
+    # else:
+    #     return render_template('algorithms_arima.html', title='ARIMA', text=text, form= form)
 
 
 
