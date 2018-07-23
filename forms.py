6fcf7d7bd614e085883c254f8d9ede983c64d426
marilia <marilia.lila.mnm@gmail.com>
2018-07-23 18:50:20 -0300

    Formatos e Novos modelos

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import InputRequired, Length
 from wtforms import SubmitField
 
@@ -14,7 +14,7 @@ class FormARIMA(FlaskForm):
     submit = SubmitField('Processar')
 
 class FormACF(FlaskForm):
-    lags = IntegerField('Lags*',default=1, validators= [InputRequired()])
+    lags = IntegerField('Lags*',default=20, validators= [InputRequired()])
 
     # CAMPOS FIXOS PARA TODOS OS FORMULÁRIOS (ARQUIVO)
     dados = FileField('Dados*', validators=[FileAllowed(['csv'], 'Somente arquivos .csv'), FileRequired()])
@@ -25,7 +25,29 @@ class FormACF(FlaskForm):
     submit = SubmitField('Processar')
 
 class FormPACF(FlaskForm):
-    lags = IntegerField('Lags',default=1, validators= [InputRequired()])
+    lags = IntegerField('Lags',default=20, validators= [InputRequired()])
+
+    # CAMPOS FIXOS PARA TODOS OS FORMULÁRIOS (ARQUIVO)
+    dados = FileField('Dados', validators=[FileAllowed(['csv'], 'Somente arquivos .csv'), FileRequired()])
+    sep = StringField('Separador', default=',', validators=[Length(min=1, max=2), InputRequired()])
+    header = BooleanField('Cabeçalho', default= True)
+    datec = IntegerField('Coluna de datas')
+
+    submit = SubmitField('Processar')
+
+class FormMA(FlaskForm):
+    window = IntegerField('Janela',default=3, validators= [InputRequired()])
+
+    # CAMPOS FIXOS PARA TODOS OS FORMULÁRIOS (ARQUIVO)
+    dados = FileField('Dados', validators=[FileAllowed(['csv'], 'Somente arquivos .csv'), FileRequired()])
+    sep = StringField('Separador', default=',', validators=[Length(min=1, max=2), InputRequired()])
+    header = BooleanField('Cabeçalho', default= True)
+    datec = IntegerField('Coluna de datas')
+
+    submit = SubmitField('Processar')
+
+class FormDecomposition(FlaskForm):
+    model = SelectField('Modelo', choices=[('adictive','Aditivo'),('multiplicative','Multiplicativo')], validators= [InputRequired()])
 
     # CAMPOS FIXOS PARA TODOS OS FORMULÁRIOS (ARQUIVO)
     dados = FileField('Dados', validators=[FileAllowed(['csv'], 'Somente arquivos .csv'), FileRequired()])
