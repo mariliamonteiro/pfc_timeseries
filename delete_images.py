51566b4eefc311c20c978a9c61553f4f29c53638
marilia <marilia.lila.mnm@gmail.com>
2018-07-29 21:09:54 -0300

    Decompose COMPLETO! e +

--- delete_images.py
+++ delete_images.py
@@ -5,17 +5,14 @@ def delete_images(days):
     cutoff = now - (days * 86400)
 
     files = os.listdir('static/images')
-    print (files)
+
     for xfile in files:
-        print (os.path.isfile( 'static/images/'+ xfile ))
         if os.path.isfile( 'static/images/'+ xfile ):
             t = os.stat( 'static/images/'+ xfile)
             c = t.st_ctime
-            print (c)
 
             # delete file if older than a week
             ext = xfile.split('.')[1]
-            print (ext)
             if ext == 'png':
                 if c < cutoff:
                     os.remove('static/images/'+ xfile)
