b6dc10cb244289e0a4fbf9a75aa3e0eedfe74755
marilia <marilia.lila.mnm@gmail.com>
2018-09-27 12:19:42 -0300

    Periodograma com gráficos de frequência e período

--- algo_periodogram.py
+++ algo_periodogram.py
@@ -10,7 +10,11 @@ import numpy as np
 UPLOAD_FOLDER = 'temp_files'
 
 def data_periodogram(dados):
-    freq = np.arange(1/len(dados), 0.5 + 1/len(dados), 1/len(dados))
+    freq = np.arange(0, 0.5, 1/len(dados))
+    periodo = 1/freq
+
+    pyplot.plot(dados)
+    pyplot.show()
 
     pgram = periodogram(dados)
     if len(pgram) % 2 == 0:
@@ -20,7 +24,7 @@ def data_periodogram(dados):
         pgram = pgram[0:int((len(dados))/2)+1]
         index = np.argmax(pgram)
 
-    freq_max = index/len(dados)
+    freq_max = freq[index]
     T = len(dados)/index
 
     df_output = pd.DataFrame({'Frequencia': freq,
@@ -32,9 +36,20 @@ def periodogram_plot(series):
     figure_name = os.path.join('static','images', filename)
 
     pgram, freq, df, index, freq_max = data_periodogram(series)
-    pyplot.plot(freq, pgram)
+
+    with np.errstate(divide='ignore'):
+        periodo = 1/freq
+
+    fig = pyplot.figure()
+    freq2 = fig.add_subplot(211)
+    periodo2 = fig.add_subplot(212)
+    freq2.plot(np.flip(periodo,0), np.flip(pgram,0), 'o')
+    pyplot.ylabel('P')
+    pyplot.xlabel('Periodo')
+
+    periodo2.plot(freq, pgram)
     pyplot.ylabel('P')
     pyplot.xlabel('Frequência')
-    pyplot.savefig(figure_name)
+    fig.savefig(figure_name)
     pyplot.close()
     return filename
